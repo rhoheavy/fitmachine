6d64c3b2e511ee8f1b7f09d16058e97ebd8a5604
Rena Salazar <renasalazart@outlook.com>
2022-11-24 10:52:49 -0700

    Viewchild

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { ElementDecorator } from 'ionicons/dist/types/stencil-public-runtime';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  scanActive = false;
+  scanResult = "";
+  @ViewChild('video', { static: false }) video: ElementRef | undefined;
 
-  constructor() {}
+  videoElement: any;
+
+  constructor(private toastCtrl: ToastController) { }
+
+  ngAfterViewInit() {
+    this.videoElement = this.video?.nativeElement;
+  }
+
+  startScan() {
+
+  }
+
+  // Helper functions
+
+  stopScan() {
+    this.scanActive = false;
+  }
+
+  reset() {
+    this.scanResult = "";
+  }
+
+  async showQrToast() {
+    const toast = await this.toastCtrl.create({
+      message: `Open ${this.scanResult}?`,
+      position: `top`,
+      buttons: [
+        {
+          text: `Open`,
+          handler: () => {
+            window.open(this.scanResult, '_system', 'location=yes');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+
+
 
 }
